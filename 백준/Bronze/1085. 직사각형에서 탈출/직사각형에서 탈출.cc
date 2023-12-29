5e3c7787e05427563ed69b1a50cc382bef4be441
euneun17 <0617ye@gmail.com>
2023-12-29 19:05:14 +0900

    [Bronze III] Title: 직사각형에서 탈출, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.cc
+++ 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.cc
@@ -5,24 +5,8 @@ int main(){
 	int a, b, c, d, x, y;
 	cin >> a >> b >> c >> d;
 
-	if (a < c - a) {
-		x = a;
-	}
-	else {
-		x = c - a;
-	}
+	x = min(a, c - a);
+	y = min(b, d - b);
 
-	if (b < d - b) {
-		y = b;
-	}
-	else {
-		y = d - b;
-	}
-
-	if (x < y) {
-		cout << x;
-	}
-	else {
-		cout << y;
-	}
+	cout << min(x, y);
 }
