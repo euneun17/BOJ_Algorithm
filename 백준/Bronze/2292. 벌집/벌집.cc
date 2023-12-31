d53ae11e9ebe36f5ead34ca4df463cec52fb77fb
euneun17 <0617ye@gmail.com>
2023-12-31 23:24:26 +0900

    [Bronze II] Title: 벌집, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2292. 벌집/벌집.cc
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    int number;
+    cin >> number;
+
+    int i = 0;
+
+    for (int sum = 2; sum <= number; i++)
+        sum += 6 * i;
+
+    if (number == 1) i = 1;
+
+    cout << i;
+
+    return 0;
+}
