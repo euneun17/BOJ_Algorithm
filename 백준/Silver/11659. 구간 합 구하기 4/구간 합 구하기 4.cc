454554806439dc3a69d1df3ff59694a738898a94
euneun17 <0617ye@gmail.com>
2024-01-04 16:08:42 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 64 ms, Memory: 2292 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
@@ -1,26 +1,24 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
 int main(){
 	int n, m;
 	scanf("%d %d", &n, &m);
-	int arr[100000];
 
-	for (int i = 0; i < n; i++) {
-		scanf("%d", &arr[i]);
-	}
-
-	int sArr[100000]; //합 배열 S
-	sArr[0] = 0;
+	int S[100000] = {}; //합 배열 S, 반드시 NULL(0) 값으로 초기화해주어야 함!! -> S[0] == 0 이기 위함
 	for (int i = 1; i < n + 1; i++) {
-		sArr[i] = sArr[i - 1] + arr[i - 1];
+		int temp;
+		scanf("%d", &temp);
+		S[i] = S[i - 1] + temp;
 	}
 
-	for (int i = 0; i < m; i++) {
-		int j, k;
-		scanf("%d %d", &j, &k);
-		cout << sArr[k] - sArr[j - 1] << "\n";
+	for (int k = 0; k < m; k++) {
+		int i, j;
+		scanf("%d %d", &i, &j);
+		cout << S[j] - S[i - 1] << "\n";
 	}
 }
