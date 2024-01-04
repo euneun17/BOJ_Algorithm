f265bd14044ec0714fce27baa84eaf58e9541198
euneun17 <0617ye@gmail.com>
2024-01-04 16:17:18 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 40 ms, Memory: 2288 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
@@ -1,24 +1,26 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-#include <string>
-#include <vector>
-
 using namespace std;
 
 int main(){
-	int n, m;
-	scanf("%d %d", &n, &m);
 
-	int S[100000] = {}; //합 배열 S, 반드시 NULL(0) 값으로 초기화해주어야 함!! -> S[0] == 0 이기 위함
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n, m;
+	cin >> n >> m;
+
+	int S[100000] = {};
 	for (int i = 1; i < n + 1; i++) {
 		int temp;
-		scanf("%d", &temp);
+		cin >> temp;
 		S[i] = S[i - 1] + temp;
 	}
 
 	for (int k = 0; k < m; k++) {
 		int i, j;
-		scanf("%d %d", &i, &j);
+		cin >> i >> j;
 		cout << S[j] - S[i - 1] << "\n";
 	}
 }
