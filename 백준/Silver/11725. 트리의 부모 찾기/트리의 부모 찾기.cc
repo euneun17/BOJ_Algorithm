fef4df6c25251c82825eed1ff631ebbce468b870
euneun17 <0617ye@gmail.com>
2024-01-28 23:28:19 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 52 ms, Memory: 12492 KB -BaekjoonHub

---
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.cc
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+vector<vector<int>> tree;
+vector<bool> visited;
+vector<int> result;
+void DFS(int node);
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n; cin >> n;
+	tree.resize(n + 1);
+	for (int i = 0; i < n - 1; i++) {
+		int a, b; cin >> a >> b;
+		tree[a].push_back(b);
+		tree[b].push_back(a);
+	}
+
+	visited.resize(n + 1);
+	result.resize(n + 1);
+
+	DFS(1);
+
+	for (int i = 2; i <= n; i++) {
+		cout << result[i] << "\n";
+	}
+}
+
+void DFS(int node) {
+	for (int i : tree[node]) {
+		if (!visited[i]) {
+			visited[i] = true;
+			result[i] = node;
+			DFS(i);
+		}
+	}
+}
