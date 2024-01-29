6afe200d5cae7ab1efab9fc9e052884649689d41
euneun17 <0617ye@gmail.com>
2024-01-29 23:57:29 +0900

    [Silver IV] Title: 문자열 집합, Time: 1296 ms, Memory: 1091020 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.cc
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.cc
@@ -5,9 +5,9 @@ using namespace std;
 
 class Node {
 public:
-	Node* next[26];
+	Node* next[26]; //Node 타입을 가리키는 26개의 포인터 원소를 가지는 포인터 배열
 	bool isEnd;
-	Node() : isEnd(false) {
+	Node() : isEnd(false) { //생성자 초기화
 		fill(next, next + 26, nullptr);
 	}
 
@@ -16,7 +16,7 @@ public:
 	}
 
 	void insert(const char* key) {
-		if (*key == NULL) isEnd = true;
+		if (*key == NULL) isEnd = true; //NULL 대신 0도 가능
 		else {
 			int next_index = *key - 'a';
 			if (next[next_index] == nullptr) {
@@ -27,10 +27,10 @@ public:
 	}
 
 	Node* find(const char* key) {
-		if (*key == NULL) return this;
+		if (*key == NULL) return this; //문자열이 존재하는 경우, 문자배열의 마지막 노드(\0 직전)를 반환
 		
 		int next_index = *key - 'a';
-		if (next[next_index] == nullptr) return nullptr;
+		if (next[next_index] == nullptr) return nullptr; //문자열이 존재하지 않는 경우(문자 배열의 끝을 의미하는 NULL(\0)을 만나지 못한 경우
 
 		return next[next_index]->find(key + 1);
 	}
@@ -57,7 +57,7 @@ int main() {
 		cin >> text;
 		Node* result = root->find(text);
 
-		if (result && result->isEnd) {
+		if (result != nullptr && result->isEnd) { //nullptr은 false를 의미, 따라서 그냥 result로 써도 됨
 			count++;
 		}
 		m--;
