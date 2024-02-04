85906398e6d8e85ab6f3fbbed116efb64e171b8d
euneun17 <0617ye@gmail.com>
2024-02-04 21:19:08 +0900

    [Gold III] Title: LCA, Time: 1492 ms, Memory: 5176 KB -BaekjoonHub

---
+++ 백준/Gold/11437. LCA/LCA.cc
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+static int n, m;
+static vector<vector<int>> tree;
+static vector<int> depth;
+static vector<int> parent;
+static vector<bool> visited;
+
+int LCA(int index1, int index2);
+void BFS(int node);
+
+int main() {
+	cin >> n;
+	tree.resize(n+1);
+	depth.resize(n+1);
+	parent.resize(n+1);
+	visited.resize(n+1);
+
+	for (int i = 0; i < n - 1; i++) {
+		int a, b; cin >> a >> b;
+		tree[a].push_back(b);
+		tree[b].push_back(a);
+	}
+
+	BFS(1); //depth 구하는 목적
+
+	cin >> m;
+
+	for (int i = 0; i < m; i++) {
+		int a, b; cin >> a >> b;
+		cout << LCA(a, b) << "\n";
+	}
+}
+
+int LCA(int a, int b) {
+	if (depth[a] < depth[b]) swap(a, b);
+
+	//a가 더 트리 아래쪽에 있는 경우로 표준화
+	while (depth[a] != depth[b]) a = parent[a];
+	while (a != b) {
+		a = parent[a];
+		b = parent[b];
+	}
+
+	return a;
+}
+
+void BFS(int node) {
+	queue<int> queue;
+	queue.push(node);
+	visited[node] = true;
+	int level = 1;
+	int now_size = 1;
+	int count = 0;
+	
+	while (!queue.empty()) {
+		for (int i : tree[queue.front()]) {
+			if (!visited[i]) {
+				queue.push(i);
+				visited[i] = true;
+				parent[i] = queue.front();
+				depth[i] = level;
+			}
+		}
+		queue.pop();
+
+		count++;
+		if (count == now_size) {
+			count = 0;
+			now_size = queue.size();
+			level++;
+		}
+	}
+}
